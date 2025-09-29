759228204e5b9bfee2485478537a9628ad5d6f55
PSKoulouris <philippe.koulouris@gmail.com>
2025-09-29 19:48:07 +0200

    blockchain 1

---
+++ web3-training/session1/Web3ProviderGanache.js
@@ -0,0 +1,11 @@
+
+import Web3 from "web3";
+var web3 = new Web3("HTTP://127.0.0.1:7545")
+console.log(web3)
+
+
+
+
+
+//web3.eth.defaultAccount = web3.eth.accounts[0];
+
---
+++ web3-training/session1/Web3ProviderSepolia.js
@@ -0,0 +1,9 @@
+
+import Web3 from "web3";
+var web3 = new Web3('https://sepolia.infura.io/v3/18e3c9187d6d4c11a36a76051c3f9b9a')
+console.log(web3)
+
+
+
+//web3.eth.defaultAccount = web3.eth.accounts[0];
+
